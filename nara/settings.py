2d258d8e794bc6c502b3b8b326f21c74815cd810
sanjay <sanjay1narayanan@gmail.com>
2020-05-09 00:27:05 -0400

    postgres adjustments

--- nara/settings.py
+++ nara/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -48,7 +49,6 @@ INSTALLED_APPS = [
     'blog',
     'stills',
     'beats',
-    'basicmodel'
 ]
 
 MIDDLEWARE = [
@@ -89,13 +89,14 @@ WSGI_APPLICATION = 'nara.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -161,4 +162,7 @@ MARKDOWNX_MARKDOWN_EXTENSIONS = [
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/')
 
-MODEL_ROOT = os.path.join(BASE_DIR, 'models')
+# MODEL_ROOT = os.path.join(BASE_DIR, 'models')
+
+# This is new
+# del DATABASES['default']['OPTIONS']['sslmode']
